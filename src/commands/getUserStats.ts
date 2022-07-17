b4d8d5d75d2c6de463ed8ee895a22ce652eae7a0
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2022-07-17 12:48:09 -0700

    use FB_AUTH_SIGNER from environment

--- src/commands/getUserStats.ts
+++ src/commands/getUserStats.ts
@@ -5,7 +5,8 @@ import { handleGenericError } from '../lib/error'
 import { getFlashbotsProvider } from '../lib/flashbots'
 
 export default class GetUserStats extends Command {
-  static description = `Get Flashbots reputation data for your account.
+  static description = `Get Flashbots reputation data for your account. 
+  You may optionally specify a private key, otherwise the environment variable FB_AUTH_SIGNER will be used.
   Note: your private key is not sent over the web. It is only used to sign a message which proves you own the account.`
 
   static examples = [
@@ -13,7 +14,7 @@ export default class GetUserStats extends Command {
 <%= config.bin %> <%= command.id %> 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`,
   ]
 
-  static args = [{name: 'signer_key', description: "Private key used to sign bundles", required: true}]
+  static args = [{name: 'signer_key', description: "Private key used to sign bundles", required: false}]
   
   async catch(e: any) {
     handleGenericError(e, this)
